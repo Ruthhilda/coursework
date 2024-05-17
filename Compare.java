4ab01b65857c5a3c670a72fc07cd2be26df92399
Ruthhilda <ruthnakawombe@gmail.com>
2024-05-17 16:16:45 +0300

    Add files via upload

---
+++ Compare.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vu.helloworld;
+
+/**
+ *
+ * @author mukis
+ */
+public class Compare {
+  // our main program start here 
+    public static void main(String[] args){
+        /*
+        equal operators : ==
+        less Than operators:<
+        Greater Than : >
+        */
+        int a =20;
+        int b=25;
+        boolean isEquals = a==b;
+        System.out.println(isEquals);
+        
+        // less than
+        boolean isLessthan =a<b;
+        System.out.println(isLessthan);
+        
+        // greater than
+        boolean isGreaterthan =a>b;
+        System.out.println(isGreaterthan);
+        
+        // IS NOT EQUAL
+        boolean isNotEqual = a!=b;
+        System.out.println(isNotEqual);
+        
+        // is less than or equal 
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }  
+}
